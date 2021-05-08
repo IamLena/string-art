b3fdee427d4e10bf02f0f6e2745b6150fdad4282
Lena <helly.luchina@gmail.com>
2021-05-08 16:12:06 +0300

    conn status, res, err

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ assert(0 <= L_min <= m/2)
 
 pins = np.zeros((m, 2), dtype='int16')
 conns = np.packbits(np.zeros((N), dtype='bool'))
-img = np.zeros((Z, Z), dtype='int8')
-# res[Z][Z]
-# err[Z][Z]
+img = np.zeros((Z, Z), dtype=np.uint8)
+res = np.full((Z, Z), 255, dtype=np.uint8)
+err = np.subtract(img, res, dtype='int16')
 
 def set_pin_coords(Z, m, a, pins):
 	angle = 0
@@ -61,16 +61,68 @@ def image_parse(img, image_path_in, image_path_out, Z):
 	img[npcrop == 255] = 255
 
 	#save
-	gr_im = Image.fromarray(img).save(image_path_out)
+	Image.fromarray(img).save(image_path_out)
 
+def find_conn_by_index(index):
+	one_type_count = N / 4
+	max_pins_in_line = m - 2 * L_min + 1
+
+	f_type = index // one_type_count
+	part_index = index % one_type_count
+	if part_index < max_pins_in_line * L_min:
+		pin_1 = part_index // max_pins_in_line
+		pin_2 = pin_1 + L_min + (part_index % max_pins_in_line)
+	else:
+		pin_1 = L_min
+		part_index -= max_pins_in_line * L_min
+		max_pins_in_this_line = max_pins_in_line - 1
+		while (part_index >= max_pins_in_this_line):
+			part_index -= max_pins_in_this_line
+			max_pins_in_this_line -= 1
+			pin_1 += 1
+		pin_2 = pin_1 + L_min + (part_index % max_pins_in_this_line)
+	return([pin_1, pin_2, f_type])
+
+def find_index_by_pins(pin_1, pin_2, type, log = 0):
+	one_type_count = N / 4
+	max_pins_in_line = m - 2 * L_min + 1
+
+	if (pin_1 > pin_2):
+		pin_1, pin_2 = pin_2, pin_1
+		if (type == 0):
+			type = 2
+		elif (type == 2):
+			type = 0
+	if (pin_1 < L_min):
+		if (pin_2 >= pin_1 + L_min and pin_2 <= NUM_OF_PINS + pin_1 - L_min + 1):
+			one_type_index = pin_1 * max_pins_in_line + pin_2 - L_min - pin_1
+			return one_type_index + one_type_count * type
+		else:
+			return -1
+	if (pin_2 >= pin_1 + L_min and pin_2 < NUM_OF_PINS):
+		index = max_pins_in_line * L_min
+		max_pins_in_this_line = max_pins_in_line - 1
+		found_pin = L_min
+		while (found_pin != pin_1):
+			found_pin += 1
+			index += max_pins_in_this_line
+			max_pins_in_this_line -= 1
+		return (index + pin_2 - L_min - pin_1) + one_type_count * type
+	else:
+		return -1
+
+old_err = err.copy
 
 set_pin_coords(Z, m, a, pins)
-image_parse(img, image_path, Z)
+image_parse(img, image_path_in, image_path_out, Z)
+err = np.subtract(img, res, dtype='int16')
 
+print(np.array_equal(old_err, err))
 
 print("pins: ", pins.nbytes, " bytes")
 print("conns: ", conns.nbytes, " bytes")
 print("img: ", img.nbytes, " bytes")
+print("err: ", err.nbytes, " bytes")
 
-sum = pins.nbytes + conns.nbytes + img.nbytes
+sum = pins.nbytes + conns.nbytes + img.nbytes + err.nbytes
 print("\nsum: ", sum, " bytes")
