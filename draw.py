0a59220f5b98fee09754e819438153827050ec7d
Lena <helly.luchina@gmail.com>
2021-05-13 18:57:31 +0300

    throwing large errors

--- draw.py
+++ draw.py
@@ -1,214 +1,271 @@
-import numpy
+from conn import *
+from tkinter import Tk, Canvas
+from tkinter.ttk import Frame, Label
+import concurrent.futures
 import math
-import tkinter
-import itertools
-from PIL import Image
+import numpy as np
+from PIL import Image, ImageDraw, ImageTk
 
-NUM_OF_PINS = 180
-DIAMETR = 60
-DIAMETR_inch = DIAMETR / 2.54
-RADIUS = DIAMETR / 2
-ANGLE_STEP = 360 / NUM_OF_PINS
-ANGLE_STEP_radians = 2 * math.pi / NUM_OF_PINS
-NUM_OF_CONNECTIONS = int(2 * math.factorial(NUM_OF_PINS) / math.factorial(NUM_OF_PINS - 2))
-STRING_WIDTH = 0.2
-RESOLUTION = DIAMETR / STRING_WIDTH #number of pixels in diametr
-CENTER_x = RESOLUTION / 2
-CENTER_y = RESOLUTION / 2
-RADIUS_pixels = RESOLUTION / 2
-RESOLUTION_ppi = RESOLUTION / DIAMETR_inch
+def show_any_img(root, canvas, nparr):
+	w = len(nparr) / 2
+	image = ImageTk.PhotoImage(Image.fromarray(nparr))
+	imagesprite = canvas.create_image(w, w, image=image)
+	canvas.pack()
+	root.update()
 
-pin_coords = []
-angle = 0
-for i in range(NUM_OF_PINS):
-	pin_coords.append([math.floor(CENTER_x + RADIUS_pixels * math.cos(angle)), math.floor(CENTER_y + RADIUS_pixels * math.sin(angle))])
-	angle += ANGLE_STEP_radians
+def get_error(pin_1, pin_2, f_type, res, img):
+	error = 0
+	len_p = 0
 
-# connections = numpy.zeros(NUM_OF_CONNECTIONS, dtype=numpy.bool8)
-NUM_OF_CONNECTIONS = int(NUM_OF_CONNECTIONS / 4)
-# connections = numpy.random.randint(0, 2, NUM_OF_CONNECTIONS)
-connections = numpy.random.choice([0, 1], size=NUM_OF_CONNECTIONS, p=[.99, .01])
-print(connections.sum())
-connections_compination = list(itertools.combinations(pin_coords, 2))
+	xk = pin_1[0]
+	xn = pin_2[0]
+	yk = pin_1[1]
+	yn = pin_2[1]
+	intensity = 255 #levels of intensity
+	curInt = intensity
 
-def draw_connection(myCanvas, connection_index):
-	conn = connections_compination[connection_index]
-	x0 = conn[0][0]
-	y0 = conn[0][1]
-	x1 = conn[1][0]
-	y1 = conn[1][1]
-	myCanvas.create_line(x0, y0, x1, y1, fill="#000", width=1)
+	dx = xk - xn
+	dy = yk - yn
 
-def draw_all_connections(myCanvas, connections):
-	for i in range(len(connections)):
-	# for i in range (30):
-		if connections[i]:
-			# print(i)
-			draw_connection(myCanvas, i)
-	myCanvas.create_oval(1, 1, RESOLUTION, RESOLUTION, dash=(1,5))
-	myCanvas.pack()
+	# color pin_coords black
+	error += int(img[yk][xk])	# res[yk][xk] -> 0
+	error += int(img[yn][xn])	# res[yn][xn] -> 0
+	len_p += 2
 
-	myCanvas.update()
-	myCanvas.postscript(file="file_name.ps", colormode='color')
-	img = Image.open("file_name.ps")
-	img.save("file.png", "png")
-
-	# black or white!
-	# print("colors")
-	# for i in range(100):
-	# 	colors = img.getpixel((i,40))
-	# 	print(colors)
-
-def C(n, k):
-	result = n
-	stop = n - k + 1
-	while n > stop:
-		n -= 1
-		result *= n
-	while k > 1:
-		result /= k
-		k -= 1
-	return int(result)
-	# result = n * (n - 1) / 2
-
-def cgen(i,n,k):
-	"""
-	returns the i-th combination of k numbers chosen from 1,2,...,n
-	"""
-	c = []
-	r = i
-	j = 0
-	for s in range(1,k+1):
-		cs = j+1
-		while r-C(n-cs,k-s)>0:
-			r -= C(n-cs,k-s)
-			cs += 1
-		if (s == k):
-			cs += r
-		c.append(cs)
-		j = cs
-	return c
-
-def color_pixel(x, y, myCanvas, color)
-	myCanvas.create_line(x, y, x + 1, y, fill=color)
-	myCanvas.pack()
-	myCanvas.update()
-
-root = tkinter.Tk()
-myCanvas = tkinter.Canvas(root, bg="white", height=RESOLUTION + 1, width=RESOLUTION + 1)
-# draw_all_connections(myCanvas, connections)
-myCanvas.create_line(pin_coords[100][0] - 2, pin_coords[100][1], pin_coords[30][0], pin_coords[30][1], fill="#000", width=1)
-myCanvas.create_line(pin_coords[100][0] + 2, pin_coords[100][1], pin_coords[30][0], pin_coords[30][1], fill="#000", width=1)
-myCanvas.create_oval(1, 1, RESOLUTION, RESOLUTION, dash=(1,5))
-myCanvas.pack()
-myCanvas.update()
-
-root.mainloop()
-
-print(C(NUM_OF_PINS, 2))
-index = 13000
-print(cgen(index, NUM_OF_PINS, 2))
-print(pin_coords[101-1])
-print(pin_coords[102-1])
-print("-----------------")
-res = connections_compination[index]
-print(res)
-print(pin_coords.index(res[0]))
-print(pin_coords.index(res[1]))
-
-
-def sign(x):
-	if (sign > 0):
-		return 1
-	if (signa < 0):
-		return -1
-	return 0
-
-def By(xn, yn, xk, yk):
-	# m, xi, yi
-	intensity = 100 #levels of intensity
-	dx = (xk - xn)
-	dy = (yk - yn)
-	color = "#000000"
-
-	color_pixel(xn, yn, myCanvas, color)
-	color_pixel(xk, yk, myCanvas, color)
-
-	if (dx == 0) {
-		sy = sign(yk - yn)
+	# vertical line
+	if (dx == 0):
+		sy = np.sign(yk - yn)
 		y = yn
-		while (y != yk) {
-			color_pixel(xn, yn, myCanvas, color)
+		while (y != yk):
+			error += int(img[y][xn])	# res[y][xn] -> 0; curInt - max
+			len_p += 1
 			y += sy
-		}
-	}
-	else if (dy == 0) {
-		sx = sign(xk - xn)
-		x = xn
-		while (x != xk) {
-			color_pixel(xn, yn, myCanvas, color)
-			x += sx
-		}
-	}
-	else if (abs(dy) <= abs(dx)) { #m < 1
-		if (dx < 0) {
-			t = xk
-			xk = xn
-			xn = t
 
-			t = yk
-			yk = yn
-			yn = t
+	# horizontal line
+	elif (dy == 0):
+		sx = np.sign(xk - xn)
+		x = xn
+		while (x != xk):
+			error += int(img[yn][x])	# res[yn][x] -> 0; curInt - max
+			len_p += 1
+			x += sx
+
+	# m < 1
+	elif (abs(dy) <= abs(dx)):
+		if (dx < 0):
+			xk, xn = xn, xk
+			yk, yn = yn, yk
 			dx = -dx
 			dy = -dy
-		}
-
 		m = dy / dx
 
 		yi = yn + m
-		for x in range(xn + 1, x < xk)
-		for (let x = xn + 1; x < xk; x += 1) {
-			let curInt = intensity - (yi % 1) * 100
-			let newColor = changeColor(color, curInt)
-			ctx.fillStyle = newColor
-			ctx.fillRect(x, Math.floor(yi), 1, 1)
-			if (curInt != 100) {
-				curInt = intensity - curInt
-				newColor = changeColor(color, curInt)
-				ctx.fillStyle = newColor
-				ctx.fillRect(x, Math.ceil(yi), 1, 1)
-			}
-			yi = yi + m
-		}
-	}
-	else {
-		if (dy < 0) {
-			let t = xn
-			xn = xk
-			xk = t
+		for x in range (xn + 1, xk, 1):
+			curInt = intensity - (yi % 1) * intensity
+			if res[math.floor(yi)][x] <= curInt:
+				error += int(img[math.floor(yi)][x])	# res -> 0
+			else:
+				error += np.abs(int(img[math.floor(yi)][x]) - (int(res[math.floor(yi)][x]) - curInt))
+			len_p += 1
 
-			t = yn
-			yn = yk
-			yk = t
+			if (curInt != intensity):
+				curInt = intensity - curInt
+				if res[math.floor(yi)+1][x] <= curInt:
+					error += int(img[math.floor(yi)+1][x])	# res -> 0
+				else:
+					error += np.abs(int(img[math.floor(yi)+1][x]) - (int(res[math.floor(yi)+1][x]) - curInt))
+				len_p += 1
+			yi = yi + m
+	# m > 1
+	else:
+		if (dy < 0):
+			xn, xk = xk, xn
+			yn, yk = yk, yn
+
 			dy = -dy
 			dx = -dx
-		}
+
 		m = dx / dy
-		console.log(`m = ${m}, dx = ${dx}, dy = ${dy}, xn = ${xn}`)
 
 		xi = xn + m
-		for (let y = yn + 1; y < yk; y += 1) {
-			let curInt =  intensity - (xi % 1) * 100
-			let newColor = changeColor(color, curInt)
-			ctx.fillStyle = newColor
-			ctx.fillRect(Math.floor(xi), y, 1, 1)
-			if (curInt != 100) {
+		for y in range (yn + 1, yk, 1):
+			curInt =  intensity - (xi % 1) * intensity
+			if res[y][math.floor(xi)] <= curInt:
+				error += int(img[y][math.floor(xi)]) 	# res -> 0
+			else:
+				error += np.abs(int(img[y][math.floor(xi)]) - (int(res[y][math.floor(xi)]) - curInt))
+			len_p += 1
+
+			if (curInt != intensity):
 				curInt = intensity - curInt
-				newColor = changeColor(color, curInt)
-				ctx.fillStyle = newColor
-				ctx.fillRect(Math.ceil(xi), y, 1, 1)
-			}
+				if (res[y][math.floor(xi)+1] <= curInt):
+					error += int(img[y][math.floor(xi)+1])	# res -> 0
+				else:
+					error += np.abs(int(img[y][math.floor(xi)+1]) - (int(res[y][math.floor(xi)+1]) - curInt))
+				len_p += 1
 			xi = xi + m
-		}
-	}
-}
+
+	error /= len_p
+	return error
+
+def try_connection(listofargs):
+	conn_index = listofargs[0]
+	pins = listofargs[1]
+	res = listofargs[2]
+	img = listofargs[3]
+	N = listofargs[4]
+	m = listofargs[5]
+	L_min = listofargs[6]
+
+	pin_1, pin_2, ftype = find_conn_by_index_coords(conn_index, N, m, L_min, pins)
+
+	new_error = get_error(pin_1, pin_2, ftype, res, img)
+	if (0 <= conn_index % 3000 <= 10):
+		print(new_error, conn_index)
+	return new_error, conn_index
+
+def draw(pin_1, pin_2, f_type, res):
+	xk = pin_1[0]
+	xn = pin_2[0]
+	yk = pin_1[1]
+	yn = pin_2[1]
+	intensity = 255 #levels of intensity
+	# intensity = 255
+	dx = xk - xn
+	dy = yk - yn
+
+	# color pin_coords black
+	res[yk][xk] = 0
+	res[yn][xn] = 0
+
+	curInt = intensity
+
+	# vertical line
+	if (dx == 0):
+		sy = np.sign(yk - yn)
+		y = yn
+		while (y != yk):
+			if (res[y][xn] <= curInt):
+				res[y][xn] = 0
+			else:
+				res[y][xn] -= curInt
+			y += sy
+
+	# horizontal line
+	elif (dy == 0):
+		sx = np.sign(xk - xn)
+		x = xn
+		while (x != xk):
+			if (res[yn][x] <= curInt):
+				res[yn][x] = 0
+			else:
+				res[yn][x] -= curInt
+			x += sx
+
+	# m < 1
+	elif (abs(dy) <= abs(dx)):
+		if (dx < 0):
+			xk, xn = xn, xk
+			yk, yn = yn, yk
+			dx = -dx
+			dy = -dy
+		m = dy / dx
+
+		yi = yn + m
+		for x in range (xn + 1, xk, 1):
+			curInt = intensity - (yi % 1) * intensity
+			if res[math.floor(yi)][x] <= curInt:
+				res[math.floor(yi)][x] = 0
+			else:
+				res[math.floor(yi)][x] -= curInt
+			if (curInt != intensity):
+				curInt = intensity - curInt
+				if res[math.floor(yi)+1][x] <= curInt:
+					res[math.floor(yi)+1][x] = 0
+				else:
+					res[math.floor(yi)+1][x] -= curInt
+			yi = yi + m
+
+	else:
+		if (dy < 0):
+			xn, xk = xk, xn
+			yn, yk = yk, yn
+
+			dy = -dy
+			dx = -dx
+
+		m = dx / dy
+
+		xi = xn + m
+		for y in range (yn + 1, yk, 1):
+			curInt =  intensity - (xi % 1) * intensity
+			if res[y][math.floor(xi)] <= curInt:
+				res[y][math.floor(xi)] = 0
+			else:
+				res[y][math.floor(xi)] -= curInt
+			if (curInt != intensity):
+				curInt = intensity - curInt
+				if (res[y][math.floor(xi)+1] <= curInt):
+					res[y][math.floor(xi)+1] = 0
+				else:
+					res[y][math.floor(xi)+1] -= curInt
+			xi = xi + m
+
+def draw_connection(conn_index, img, res,  N, m, L_min, pins):
+	pin_1, pin_2, ftype = find_conn_by_index_coords(conn_index, N, m, L_min, pins)
+	draw(pin_1, pin_2, ftype, res)
+
+def draw_image(img, res, conns, pins, N, n, m, L_min):
+	root = Tk()
+	w = len(img)
+	canvas = Canvas(root,width=w,height=w)
+	canvas.pack()
+
+	for j in range(n):
+		conn_index = -1
+		minerr = -1
+		errors = []
+
+		with concurrent.futures.ProcessPoolExecutor() as executor:
+			filter = []
+			for i in range(N):
+				if (conns[i] == 0):
+					filter.append([i, pins, res, img, N, m, L_min])
+			errors = executor.map(try_connection, filter)
+			errors = list(errors)
+			errors.sort()
+			print("errorlen = ", len(errors))
+
+			minerr = errors[0][0]
+			conn_index = errors[0][1]
+			# for er in errors:
+			# 	if er[0] < minerr:
+			# 		minerr = er[0]
+			# 		conn_index = er[1]
+
+			# print('min', minerr, conn_index)
+			# exit(0)
+			# nperr = np.array(errors, shape=())
+			# conn_index = errors.index(min(errors))
+			print(f'conn #{conn_index}\t: error = {minerr}')
+
+		if len(errors) == 0:
+			break
+
+		conns[conn_index] = 1
+		print("drawing ", j, " conn # ", conn_index, " err = ", minerr)
+		draw_connection(conn_index, img, res, N, m, L_min, pins)
+		show_any_img(root, canvas, res)
+		for i in range (len(errors)):
+			if (len(errors) - i < 100 or errors[i][0] > 100):
+				conns[errors[i][1]] = 2
+				print("throwing ", errors[i])
+
+		# conns[conn_index] = 1
+		# print("drawing ", conn_index)
+		# draw_connection(conn_index, img, res, N, m, L_min, pins)
+		# show_any_img(root, canvas, res)
+
+	print(j, "connections are made")
+	print("conns\n", conns)
+	Image.fromarray(res).save("pics/res_portrait.png")
