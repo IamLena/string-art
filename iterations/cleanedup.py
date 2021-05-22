a38702f9622c572b9734fb96654f2a0c8fbd56c8
Lena <helly.luchina@gmail.com>
2021-05-22 21:16:26 +0300

    cleaned up, removed all to arhive

--- iterations/cleanedup.py
+++
@@ -1,9 +0,0 @@
-from init import *
-from draw import *
-import time
-
-t1 = time.perf_counter()
-res_conn = draw_image(img, res, conns, pins, N, n, m, L_min)
-t2 = time.perf_counter()
-
-print(f'Finished in {t2-t1} seconds')
--- past/README.md
+++
@@ -1,2 +0,0 @@
-# string-art
-diplom
